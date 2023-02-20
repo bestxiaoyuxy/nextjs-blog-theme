bc6b481eb32fc180983279f544d62bd843c348f7
xiaoyu <bestxiaoyuxy@gmail.com>
2023-02-21 00:09:59 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : '久欧朋静态博客';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : '久欧朋&&忆光';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Powered By <a href="https://www.eray.cc">忆光</a>';
 
   return {
     name,
